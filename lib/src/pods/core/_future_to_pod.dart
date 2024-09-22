073ebbadda6546ac51f7d15e2ac4f79997363d83
robmllze <86869208+robmllze@users.noreply.github.com>
2024-09-22 22:39:28 +1000

    +feat: Add FutureToPod

--- lib/src/pods/core/_future_to_pod.dart
+++ lib/src/pods/core/_future_to_pod.dart
@@ -10,27 +10,17 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
 
-import 'dart:async';
+// ignore_for_file: invalid_use_of_visible_for_testing_member
 
-import 'package:df_type/df_type.dart';
-import 'package:flutter/foundation.dart';
+part of 'core.dart';
 
-import '/src/_mixins/protected_pod_mixin.dart';
-import '/src/_index.g.dart';
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-part '_child_pod_base.dart';
-part '_child_pod.dart';
-part '_future_to_pod.dart';
-part '_generic_pod_mixin.dart';
-part '_pod_notifier.dart';
-part '_reducer_pod.dart';
-part '_root_pod.dart';
-
-part '_reducers/multiple_pod_reducer.dart';
-part '_reducers/pod_reducer_1.dart';
-part '_reducers/pod_reducer_2.dart';
-part '_reducers/pod_reducer_3.dart';
-part '_reducers/pod_reducer_4.dart';
-part '_reducers/pod_reducer_5.dart';
-part '_reducers/pod_reducer_6.dart';
-part '_reducers/pod_reducer_7.dart';
+/// A Pod that is initially `null` then sets its [value] when a [Future]
+/// completes.
+base class FutureToPod<T> extends PodNotifier<T?> with GenericPod<T?> {
+  final Future<T> future;
+  FutureToPod(this.future) : super(null) {
+    future.then((value) => _set(value));
+  }
+}
