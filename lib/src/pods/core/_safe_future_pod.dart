b29dbe3972924604de16d217c47820808b65137d
robmllze <86869208+robmllze@users.noreply.github.com>
2025-02-10 21:33:07 +1100

    +breaking: Update dependencies

--- lib/src/pods/core/_safe_future_pod.dart
+++ lib/src/pods/core/_safe_future_pod.dart
@@ -16,14 +16,18 @@ part of 'core.dart';
 
 /// A Pod that is initially [None] then sets its [value] when a [FutureOr]
 /// completes as [Ok] or fails as [Err].
-base class SafeFuturePod<T> extends PodNotifier<Option<Result<T, Object>>>
-    with GenericPod<Option<Result<T, Object>>> {
+base class SafeFuturePod<T extends Object> extends PodNotifier<Option<Result<T>>>
+    with GenericPod<Option<Result<T>>> {
   /// Creates a [SafeFuturePod] with the specified [future].
   SafeFuturePod(FutureOr<T> future) : super(const None()) {
     consec(
       future,
       (e) => _set(Some(Ok(e))),
-      onError: (e) => _set(Some(Err(e))),
+      onError: (e) => _set(
+        Some(
+          Err(stack: ['SafeFuturePod'], error: e),
+        ),
+      ),
     );
   }
 
@@ -31,5 +35,5 @@ base class SafeFuturePod<T> extends PodNotifier<Option<Result<T, Object>>>
   SafeFuturePod.value(T value) : super(Some(Ok(value)));
 
   /// Unwraps the Pod's value.
-  T unwrap() => value.some.unwrap().ok.value;
+  T unwrap() => value.unwrap().unwrap();
 }
